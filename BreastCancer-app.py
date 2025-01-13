f70c223cef4f13354d696ca35c02a2030cf83ef0
izza kamila <80920018+izzaKamila@users.noreply.github.com>
2025-01-13 12:19:48 +0700

    Update BreastCancer-app.py

--- BreastCancer-app.py
+++ BreastCancer-app.py
@@ -229,10 +229,13 @@ if (selected == 'Random Forest'):
         cancer_pred = rf_model.predict(input_features)
         
         if (cancer_pred[0] == 2):
-            st.success(cancer_type = 'Benign')
+            cancer_type = 'Benign'
         else:
-            st.error(cancer_type = 'Malignant')
+            cancer_type = 'Malignant'
         
         st.success(cancer_type)
-
+        if cancer_type == 'Malignant':
+            st.markdown('<h2 style="color:red;">Prediksi: Malignant</h2>', unsafe_allow_html=True)
+        else:
+            st.markdown('<h2 style="color:green;">Prediksi: Benign</h2>', unsafe_allow_html=True)
 
